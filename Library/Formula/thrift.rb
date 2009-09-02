2addbae49c4752f2d7fa9db0f79933d5d6438a46
Ask Solem <askh@opera.com>
2009-09-02 13:02:00 +0200

    thrift: Something wrong with zlib in current head, so compile --without-zlib

--- Library/Formula/thrift.rb
+++ Library/Formula/thrift.rb
@@ -13,6 +13,12 @@ import sys
 print(sys.executable.split("/bin")[0])
 '
 
+PYTHON_GET_DEPLOYMENT_TARGET = '
+from distutils.sysconfig import parse_makefile, get_makefile_filename
+m = parse_makefile(get_makefile_filename(), {})
+print(m.get("MACOSX_DEPLOYMENT_TARGET"))
+'
+
 PATCH1='
 --- /lib/cpp/src/concurrency/PosixThreadFactory.cpp	2008-11-24 14:36:45.000000000 -0800
 +++ /lib/cpp/src/concurrency/PosixThreadFactory.cpp	2008-11-24 16:41:30.000000000 -0800
@@ -29,29 +35,25 @@ PATCH1='
 
 class Thrift <Formula
   @homepage='http://incubator.apache.org/thrift/'
-  @head='http://svn.apache.org/repos/asf/incubator/thrift/trunk'
-
-  def download_strategy
-      SubversionDownloadStrategy
-  end
+  @head='git://github.com/dreiss/thrift.git'
 
   def pyprefix
     `python -c '#{PYTHON_GET_PREFIX}'`.strip()
   end
 
   def deployment_target
-      `sw_vers -productVersion`
+      `python -c '#{PYTHON_GET_DEPLOYMENT_TARGET}'`.strip()
   end
 
   def env
-      "env MACOSX_DEPLOYMENT_TARGET=#{deployment_target} PY_PREFIX=#{pyprefix}"
+      "env MACOSX_DEPLOYMENT_TARGET= PY_PREFIX=#{pyprefix}"
   end
 
   def install
     system "cp /usr/X11/share/aclocal/pkg.m4 aclocal"
     system "echo '#{PATCH1}' | patch -p1"
     system "#{env} bash bootstrap.sh"
-    system "#{env} ./configure --without-java --prefix='#{prefix}' --libdir='#{lib}'"
+    system "#{env} ./configure --without-zlib --without-java --prefix='#{prefix}' --libdir='#{lib}'"
     system "#{env} make"
     system "#{env} make install"
   end
